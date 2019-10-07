89cbc378ae70301a19270aaae50b46cc455f0ba2
mtldong@gmail.com <mtldong@gmail.com>
2019-10-07 10:17:23 -0400

    initiate local database connection

--- thestars/settings.py
+++ thestars/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'thestars.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "thestars",
+        'USER': 'root',
+        'PASSWORD': "d6N3Z2h8",
+        'HOST': "127.0.0.1",
+        'OPTIONS': { 'init_command': 'SET default_storage_engine=INNODB;' }
     }
 }
 
