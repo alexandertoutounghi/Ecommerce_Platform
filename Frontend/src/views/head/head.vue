60ad861d21ba11e2831dbe6e91148c54c89d2f72
Maude <maude.braunstein@gmail.com>
2019-11-08 14:54:42 -0500

    Removed image cycling in product details - will need to be fixed later

--- Frontend/src/views/head/head.vue
+++ Frontend/src/views/head/head.vue
@@ -290,7 +290,6 @@ export default {
         .then(response => {
           console.log(response);
           this.allMenuLabel = response.data.filter(category => !category.is_tab && category.name.includes("_Test"))
-          // this.allMenuLabel = response.data;
         })
         .catch(function(error) {
           this.allMenuLabel = []
