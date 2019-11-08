40f03d5ee0cb6fd10007f113953d085b885a9b94
Maude <maude.braunstein@gmail.com>
2019-11-08 14:19:16 -0500

    Filter out any categories that do not contain "_Test"

--- Frontend/src/views/head/head.vue
+++ Frontend/src/views/head/head.vue
@@ -289,7 +289,8 @@ export default {
       })
         .then(response => {
           console.log(response);
-          this.allMenuLabel = response.data;
+          this.allMenuLabel = response.data.filter(category => !category.is_tab && category.name.includes("_Test"))
+          // this.allMenuLabel = response.data;
         })
         .catch(function(error) {
           this.allMenuLabel = []
