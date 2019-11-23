7461a31aef294bd59b1f1c3ba3c3a354cf7a2c75
Alexander Cohen <alex.toutongi@gmail.com>
2019-11-23 18:53:24 -0500

    Revert "complete translation"

--- Frontend/src/views/list/list-sort/listSort.vue
+++ Frontend/src/views/list/list-sort/listSort.vue
@@ -2,8 +2,8 @@
     <div class="sort">
         <div class="bd">
             <form name="listform">
-                <!-- <a title="Sold" href="http://sx.web51.youxueshop.com/category.php?category=2&amp;display=grid&amp;brand=0&amp;price_min=0&amp;price_max=0&amp;filter_attr=0&amp;page=1&amp;sort=sales_volume_base&amp;order=ASC#goods_list" class="curr" rel="nofollow"><span class="search_DESC">Sold</span></a>
-                <a title="Price" href="http://sx.web51.youxueshop.com/category.php?category=2&amp;display=grid&amp;brand=0&amp;price_min=0&amp;price_max=0&amp;filter_attr=0&amp;page=1&amp;sort=shop_price&amp;order=ASC#goods_list" rel="nofollow"><span class="">Price</span></a> -->
+                <!-- <a title="销量" href="http://sx.web51.youxueshop.com/category.php?category=2&amp;display=grid&amp;brand=0&amp;price_min=0&amp;price_max=0&amp;filter_attr=0&amp;page=1&amp;sort=sales_volume_base&amp;order=ASC#goods_list" class="curr" rel="nofollow"><span class="search_DESC">销量</span></a>
+                <a title="价格" href="http://sx.web51.youxueshop.com/category.php?category=2&amp;display=grid&amp;brand=0&amp;price_min=0&amp;price_max=0&amp;filter_attr=0&amp;page=1&amp;sort=shop_price&amp;order=ASC#goods_list" rel="nofollow"><span class="">价格</span></a> -->
                 <a title="Quantity Sold" class="curr" rel="nofollow" @click="sortType('-sold_num')"><span :class="{search_DESC: cur==='-sold_num'}">Quantity Sold</span></a>
                 <a title="Price" class="curr" rel="nofollow" @click="sortType('-shop_price')"><span :class="{search_DESC: cur==='-shop_price'}">Price</span></a>
                 <!-- <input type="hidden" name="category" value="2">
