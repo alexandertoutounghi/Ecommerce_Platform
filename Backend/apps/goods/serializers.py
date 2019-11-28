3efdb648a41cc38d721cc5f202d8e090978ceb10
Alexander Cohen <alex.toutongi@gmail.com>
2019-11-27 21:18:49 -0500

    Update serializers.py

--- Backend/apps/goods/serializers.py
+++ Backend/apps/goods/serializers.py
@@ -1,7 +1,7 @@
+from django.db.models import Q
 from rest_framework import serializers
-
-from .models import Goods, GoodsCategory, GoodsImage, Banner, GoodsCategoryBrand, HotSearchWords
-
+from .models import Goods, GoodsCategory, GoodsImage, Banner, GoodsCategoryBrand, HotSearchWords, IndexAd, \
+    ProductRating
 
 # https://www.django-rest-framework.org/api-guide/serializers/
 class CategorySerializer3(serializers.ModelSerializer):
@@ -84,3 +84,13 @@ class IndexCategorySerializer(serializers.ModelSerializer):
         model = GoodsCategory
         fields = "__all__"
 
+
+class ProductRatingSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+    add_time = serializers.DateTimeField(read_only=True, format='%Y-%m-%d %H:%M')
+
+    class Meta:
+        model = ProductRating
+        fields = ("goods", "user", "rating_star", "subject", "message", "add_time")
