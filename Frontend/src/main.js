a4fbaa0945f6fc9f58de1ded82e272cb4c535c1a
Alex Cohen <alex.toutongi@gmail.com>
2019-11-27 19:05:43 -0500

    updated main.js with the imports for boostrap and fontawesome

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,3 +1,35 @@
+// Bootstrap-Vue, and FontAwesome Additions
+// ===============================================================================================
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUserCircle,
+  faStar,
+  faThumbsUp,
+  faThumbsDown
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  BFormTextarea,
+  BContainer,
+  BRow,
+  BCol,
+  BButton,
+  BProgress
+} from "bootstrap-vue";
+
+Vue.component("b-container", BContainer);
+Vue.component("b-form-textarea", BFormTextarea);
+Vue.component("b-row", BRow);
+Vue.component("b-col", BCol);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component("b-button", BButton);
+Vue.component("b-progress", BProgress);
+
+library.add(faUserCircle, faStar, faThumbsUp, faThumbsDown);
+
+// ===============================================================================================
 import mock from "../mock/mock.js";
 import $ from "jquery";
 import Vue from "vue";
